a8166d7e65a275041e2fc8b209f644f453a6835b
F4ELL <ferreiraperes1234@gmail.com>
2022-04-28 01:00:34 -0300

    feat: render listItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import * as C from './App.styles'
+import { ListItem } from './components/ListItem'
 import { TTask } from './types/item'
 
 const App = () => {
 
-  const [ list, setList ] = useState<TTask[]>([])
+  const [ list, setList ] = useState<TTask[]>([
+    { id: 1, name: 'tirar o lixo', isDone: false },
+    { id: 2, name: 'estudar javascript', isDone: true }
+  ])
 
   return(
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+          {list.map((item, index) => (
+            <ListItem key={index} item={item}/>
+          ))}
+
       </C.Area>
     </C.Container>
   )
