0d2a7779a0436aa9b7eece54545652f16bcede12
F4ELL <ferreiraperes1234@gmail.com>
2022-04-28 01:30:15 -0300

    feat: create add tasks function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import * as C from './App.styles'
+import { AddArea } from './components/AddArea'
 import { ListItem } from './components/ListItem'
 import { TTask } from './types/item'
 
@@ -10,10 +11,22 @@ const App = () => {
     { id: 2, name: 'estudar javascript', isDone: true }
   ])
 
+  const handleAddTask = (taskName: string) => {
+    const newList = [...list]
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      isDone: false
+    })
+    setList(newList)
+  }
+
   return(
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+          <AddArea onEnter={handleAddTask} />
 
           {list.map((item, index) => (
             <ListItem key={index} item={item}/>
